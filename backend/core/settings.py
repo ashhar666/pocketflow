e3ce6826d0cebe8c75d4b05cf7bea44cacd1c8af
ashhar <ashharshahan666@gmail.com>
2026-04-28 01:58:39 +0530

    feat: disable Django admin panel - remove from urls and installed apps

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -109,11 +109,8 @@ if DEBUG:
     CSRF_TRUSTED_ORIGINS.extend(tunnels)
 
 DJANGO_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
@@ -142,11 +139,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
